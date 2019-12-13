8384bcc6058ca235e651898bd34b849086ed3ce7
Eric Schuster <eric.schuster@hotmail.com>
2019-12-13 21:08:01 +0100

    Changes to synthesizer, added AM synth, working on threading and melodygen

--- cpp_basics/simpleSine/synthesizer.cpp
+++ cpp_basics/simpleSine/synthesizer.cpp
@@ -17,74 +17,71 @@ Synthesizer::~Synthesizer() {
 }
 
 
-int Synthesizer::additiveSynthesis(int osc1, int freq1, int osc2, int freq2,char* argv)
+int Synthesizer::additiveSynthesis(float osc1, float freq1, float osc2, float freq2)
 {
   JackModule jack;
-  jack.init(argv);
+  jack.init();
   double samplerate = jack.getSamplerate();
-  Saw oscillator1;
-  Sine oscillator2;
-  // if (osc1==1){
-  //   Sine oscillator1;
-  // } else if (osc1==2){
-  //   Saw oscillator1;
-  // } else if (osc1==3){
-  //   Square oscillator1;
-  // }
-  //
-  // if (osc2==1){
-  //   Sine oscillator2;
-  // } else if (osc2==2){
-  //   Saw oscillator2;
-  // } else if (osc2==3){
-  //   Square oscillator2;
-  // }
-  oscillator1.setFrequency(freq1);
-  oscillator2.setFrequency(freq2);
-  // assign a function to the JackModule::onProces
-  jack.onProcess = [&](jack_default_audio_sample_t *inBuf,
-     jack_default_audio_sample_t *outBuf, jack_nframes_t nframes) {
+    Saw oscillator1;
+    Square oscillator2;
+    oscillator1.setFrequency(freq1);
+    oscillator2.setFrequency(freq2);
+    // assign a function to the JackModule::onProces
+    jack.onProcess = [&](jack_default_audio_sample_t *inBuf,
+       jack_default_audio_sample_t *outBuf, jack_nframes_t nframes) {
 
-    for(unsigned int i = 0; i < nframes; i++) {
-      outBuf[i] = oscillator1.getSample() + oscillator2.getSample();
-      oscillator1.tick(samplerate);
-      oscillator2.tick(samplerate);
-    }
-    return 0;
-  };
-  jack.autoConnect();
-  // JackModule jack;
-  //
-  // // init the jack, use program name as JACK client name
-  // jack.init(argv);
-  // double samplerate = jack.getSamplerate();
-  //
-  // Sine sine;
-  // Saw saw;
-  // sine.setFrequency(200);
-  // saw.setFrequency(300);
+      for(unsigned int i = 0; i < nframes; i++) {
+        outBuf[i] = oscillator1.getSample() + oscillator2.getSample();
+        oscillator1.tick(samplerate);
+        oscillator2.tick(samplerate);
+      }
+      return 0;
+    };
+    jack.autoConnect();
 
-  // assign a function to the JackModule::onProces
-  // jack.onProcess = [&](jack_default_audio_sample_t *inBuf,
-  //    jack_default_audio_sample_t *outBuf, jack_nframes_t nframes) {
-  //
-  //   for(unsigned int i = 0; i < nframes; i++) {
-  //     outBuf[i] = sine.getSample() + saw.getSample();
-  //     sine.tick(samplerate);
-  //     saw.tick(samplerate);
-  //   }
-  //   return 0;
-  // };
   std::cout << "\n\nPress 'q' when you want to quit the program.\n";
   bool running = true;
-  while (running)
-  {
+  while (running){
     switch (std::cin.get()) {
       case 'q':
         running = false;
-        // jack.end();
+        jack.end();
         break;
     }
-  }
+}
+  return 0;
+}
+
+int Synthesizer::AMSynth(float freq1, float freq2)
+{
+  JackModule jack;
+  jack.init();
+  double samplerate = jack.getSamplerate();
+    Sine oscillator1;
+    Sine oscillator2;
+    oscillator1.setFrequency(freq1);
+    oscillator2.setFrequency(freq2);
+    // assign a function to the JackModule::onProces
+    jack.onProcess = [&](jack_default_audio_sample_t *inBuf,
+       jack_default_audio_sample_t *outBuf, jack_nframes_t nframes) {
+
+      for(unsigned int i = 0; i < nframes; i++) {
+        outBuf[i] = oscillator1.getSample() + ((oscillator2.getSample()+1)/2);
+        oscillator1.tick(samplerate);
+        oscillator2.tick(samplerate);
+      }
+      return 0;
+    };
+    jack.autoConnect();
+
+  bool running = true;
+  while (running){
+    switch (std::cin.get()) {
+      case 'q':
+        running = false;
+        jack.end();
+        break;
+    }
+}
   return 0;
 }
--- cpp_basics/simpleSine/synthesizer.h
+++ cpp_basics/simpleSine/synthesizer.h
@@ -11,7 +11,8 @@ public:
   ~Synthesizer();
 int argc;
 char **argv;
-int additiveSynthesis(int osc1, int freq1, int osc2, int freq2, char* argv);
+int additiveSynthesis(float osc1, float freq1, float osc2, float freq2);
+int AMSynth(float freq1, float freq2);
 protected:
 };
 
